ff1d1b59cef515b777713a25886ba440cdadb18e
Евгений <eter55@yandex.ru>
2019-06-10 20:32:49 +0300

    test project

---
+++ Auth_Template.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Auth_Template.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestWorkflow()
+        {
+        }
+    }
+}
